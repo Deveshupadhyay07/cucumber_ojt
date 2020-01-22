9c67fcd1c7531123dad4f8a400f334107acc3114
Devesh Upadhyay <59552850+Deveshupadhyay07@users.noreply.github.com>
2020-01-22 17:49:13 +0530

    cucumber_ojt@4:30
    Done

---
+++ src/test/java/stepDefinations/StepDefination.java
@@ -0,0 +1,86 @@
+package stepDefinations;
+
+import org.openqa.selenium.WebDriver;
+
+import CommonFunLibrary.FunctionERP;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.When;
+
+public class StepDefination {
+	WebDriver driver;
+
+	@When("^Open Browser$")
+	public void open_Browser() throws Throwable {
+		driver=FunctionERP.startBrowser("chrome");
+
+		
+	}
+
+	@When("^Open Application$")
+	public void open_Application() throws Throwable {
+		FunctionERP.openApplication(driver);
+		
+	}
+
+	@When("^Wait For Username$")
+	public void wait_For_Username() throws Throwable {
+		FunctionERP.waitForElement(driver, "name", "username", "10");
+
+		
+	}
+
+	@When("^Enter Username$")
+	public void enter_Username() throws Throwable {
+		FunctionERP.typeAction(driver, "name", "username", "admin");
+	
+	}
+
+	@When("^Wait For Password$")
+	public void wait_For_Password() throws Throwable {
+		FunctionERP.waitForElement(driver, "name", "password", "10");
+		
+	}
+
+	@When("^Enter Password$")
+	public void enter_Password() throws Throwable {
+		FunctionERP.typeAction(driver, "name", "password", "master");
+		
+	}
+
+	@When("^Click On Login$")
+	public void click_On_Login() throws Throwable {
+		FunctionERP.clickAction(driver, "id", "btnsubmit");
+		
+	}
+
+	@When("^Wait For Logout$")
+	public void wait_For_Logout() throws Throwable {
+		FunctionERP.waitForElement(driver, "id", "logout", "30");
+		
+	}
+
+	@When("^Click On Logout$")
+	public void click_On_Logout() throws Throwable {
+		FunctionERP.clickAction(driver, "id", "logout");
+		
+	}
+
+	@When("^Wait For OK$")
+	public void wait_For_OK() throws Throwable {
+		FunctionERP.waitForElement(driver, "xpath", "//*[text()='OK!']", "20");
+		
+	}
+
+	@When("^Click On Ok$")
+	public void click_On_Ok() throws Throwable {
+		FunctionERP.clickAction(driver, "xpath", "//*[text()='OK!']");
+		
+	}
+
+	@When("^CloseBrowser$")
+	public void closebrowser() throws Throwable {
+		FunctionERP.closeBrowser(driver);
+		
+	}
+
+}
---
+++ target/cucumber-reports/report.js
@@ -0,0 +1,173 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "stock accounting login validation",
+  "description": "",
+  "id": "stock-accounting-login-validation",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "ApplicationLogin Validation",
+  "description": "",
+  "id": "stock-accounting-login-validation;applicationlogin-validation",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "Open Browser",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "Open Application",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "Wait For Username",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "Enter Username",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "Wait For Password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "Enter Password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "Wait For Password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "Click On Login",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "Wait For Logout",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "Click On Logout",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 15,
+  "name": "Wait For OK",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 16,
+  "name": "Click On Ok",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 17,
+  "name": "CloseBrowser",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefination.open_Browser()"
+});
+formatter.result({
+  "duration": 6573674147,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.open_Application()"
+});
+formatter.result({
+  "duration": 10104019256,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.wait_For_Username()"
+});
+formatter.result({
+  "duration": 1033594855,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.enter_Username()"
+});
+formatter.result({
+  "duration": 372513587,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.wait_For_Password()"
+});
+formatter.result({
+  "duration": 48421117,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.enter_Password()"
+});
+formatter.result({
+  "duration": 131519083,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.wait_For_Password()"
+});
+formatter.result({
+  "duration": 45081375,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.click_On_Login()"
+});
+formatter.result({
+  "duration": 616475190,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.wait_For_Logout()"
+});
+formatter.result({
+  "duration": 85761204,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.click_On_Logout()"
+});
+formatter.result({
+  "duration": 198103123,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.wait_For_OK()"
+});
+formatter.result({
+  "duration": 355423761,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.click_On_Ok()"
+});
+formatter.result({
+  "duration": 534352114,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefination.closebrowser()"
+});
+formatter.result({
+  "duration": 1655591568,
+  "status": "passed"
+});
+});
